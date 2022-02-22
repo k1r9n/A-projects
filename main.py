6a9f6ab70e14615dc1b9b5fd1422cc3c3c108350
k1r9n <pavlidi.03@mail.ru>
2022-02-22 10:12:47 +0300

    п

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from webdriver_manager.chrome import ChromeDriverManager
 import constants
+import time
+import xlrd
+import xlwt
 
 s = Service(ChromeDriverManager().install())  # это установка драйвера
 options = webdriver.ChromeOptions()
