25c157c0c1b0f5dd328ba7563e1d263839870062
k1r9n <pavlidi.03@mail.ru>
2022-02-24 01:37:15 +0300

    п

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ book = openpyxl.Workbook()
 sheet = book.active
 row = 1
 sheet.cell(row=row, column=1).value = 'Адрес'
-sheet.cell(row=row, column=2).value = 'Цена'
-sheet.cell(row=row, column=3).value = 'Ссылка'
+sheet.cell(row=row, column=2).value = 'Квадратура'
+sheet.cell(row=row, column=3).value = 'Этаж'
+sheet.cell(row=row, column=4).value = 'Цена'
+sheet.cell(row=row, column=5).value = 'Ссылка'
+
 row += 1
 
 s = Service(ChromeDriverManager().install())  # это установка драйвера
@@ -24,12 +27,19 @@ options.add_argument('headless')
 options.add_argument('window-size=1366x768')
 driver = webdriver.Chrome(service=s)
 
+def getTitle(text):
+    text = text[text.find(',')+2::]
+    square = float((text[:text.find('м')-1:]).replace(',', '.'))
+    text = text[text.find(',') + 2::]
+    floor = text[:text.find('э')-1:]
+    return square, floor
+
 try:
     driver.get(constants.URL)  # ссылка на сайт который парсим
     time.sleep(1)
     data = []
     while True:
-
+        name = driver.find_elements(By.XPATH, constants.name)
         pricelist = driver.find_elements(By.XPATH, constants.pricelist)
         adress = driver.find_elements(By.XPATH, constants.adress)
         href = driver.find_elements(By.XPATH, constants.href)
@@ -46,7 +56,8 @@ try:
             pass
         # print(len(pricelist))
         for i in range(len(pricelist)):
-            data.append([adress[i].text, pricelist[i].text, href[i].get_attribute('href')])
+            (square, floor) = getTitle(name[i].text)
+            data.append([adress[i].text, square, floor, pricelist[i].text, href[i].get_attribute('href')])
             # sheet.cell(row=row, column=1).value = adress[i].text
             # sheet.cell(row=row, column=2).value = pricelist[i].text
             # sheet.cell(row=row, column=3).value = href[i].get_attribute('href')
@@ -61,11 +72,13 @@ try:
             time.sleep(5)
         except:
             break
-    data.sort(key = lambda x:x[1])
+    data.sort(key = lambda x:x[3])
     for i in range(len(data)):
         sheet.cell(row=i+2, column=1).value = data[i][0]
         sheet.cell(row=i+2, column=2).value = data[i][1]
         sheet.cell(row=i+2, column=3).value = data[i][2]
+        sheet.cell(row=i+2, column=4).value = data[i][3]
+        sheet.cell(row=i+2, column=5).value = data[i][4]
 
     book.save("C://Users/k1r9n/Desktop/parser.xlsx")
     book.close()
